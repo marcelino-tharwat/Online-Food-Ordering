065c912e186775131bf483f5b3f7503db14b0e48
marclenotharwaat <marclenotharwat@gmail.com>
2026-05-27 16:28:07 +0300

    fix: prevent multiple add-to-cart requests on rapid clicks

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,8 +5,10 @@ import {
   setLoading,
   setError,
   optimisticAddItem,
+  optimisticRemoveItem,
 } from "../redux/slices/cartSlice";
 import type { AppDispatch } from "../redux/store";
+import { useState } from "react";
 
 interface ProductCardProps {
   id: string;
@@ -23,8 +25,32 @@ export default function ProductCard({
 }: ProductCardProps) {
   const { t } = useTranslation();
   const dispatch = useDispatch<AppDispatch>();
+  const [isAdding, setIsAdding] = useState(false);
+
+  // const handleAddToCart = async () => {
+  //   const product = {
+  //     _id: id,
+  //     name: { en: name, ar: name },
+  //     price,
+  //     image,
+  //   };
+
+  //   dispatch(optimisticAddItem({ productId: id, product }));
+
+  //   try {
+  //     dispatch(setLoading(true));
+  //     dispatch(setError(null));
+  //     await cartService.addToCart(id, 1);
+  //   } catch {
+  //     dispatch(setError(t("cart.addItemError")));
+  //   } finally {
+  //     dispatch(setLoading(false));
+  //   }
+  // };
 
   const handleAddToCart = async () => {
+    if (isAdding) return;
+
     const product = {
       _id: id,
       name: { en: name, ar: name },
@@ -32,15 +58,26 @@ export default function ProductCard({
       image,
     };
 
-    dispatch(optimisticAddItem({ productId: id, product }));
-
     try {
+      setIsAdding(true);
+
       dispatch(setLoading(true));
       dispatch(setError(null));
+
+      dispatch(
+        optimisticAddItem({
+          productId: id,
+          product,
+        }),
+      );
+
       await cartService.addToCart(id, 1);
     } catch {
       dispatch(setError(t("cart.addItemError")));
+
+      dispatch(optimisticRemoveItem(id));
     } finally {
+      setIsAdding(false);
       dispatch(setLoading(false));
     }
   };
@@ -65,11 +102,23 @@ export default function ProductCard({
           </div>
         </div>
 
-        <button
+        {/* <button
           onClick={handleAddToCart}
           className="w-full bg-white text-[#0b3b24] py-2.5 px-4 rounded-full font-bold text-xs md:text-sm hover:bg-yellow-100 active:scale-[0.98] transition-all duration-200 shadow-md flex justify-center items-center"
         >
           {t("checkout.orderNow")}
+        </button> */}
+        <button
+          onClick={handleAddToCart}
+          disabled={isAdding}
+          className={`w-full py-2.5 px-4 rounded-full font-bold text-xs md:text-sm transition-all duration-200 shadow-md flex justify-center items-center
+  ${
+    isAdding
+      ? "bg-gray-300 text-gray-500 cursor-not-allowed"
+      : "bg-white text-[#0b3b24] hover:bg-yellow-100 active:scale-[0.98]"
+  }`}
+        >
+          {isAdding ? t("cart.adding") : t("checkout.orderNow")}
         </button>
       </div>
     </div>
